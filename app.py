339d173945fad9ab752cdbcf23f900e2560518bb
aprasanna441 <aprasanna441@gmail.com>
2023-07-25 09:09:17 +0545

    Connected to Sqlite using SQLALchemy

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask,render_template,request
-
+from flask import render_template,request,redirect,Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
-registrant_name={}
+class Sports(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sport=db.Column(db.String(80),nullable=False)
+    name=db.Column(db.String(80),nullable=False)
+
+    def __repr__(self):
+        return f"<Person {self.name}>"
+    
+with app.app_context():
+    db.create_all()
+
+
+
 
 @app.route('/')
 def index():
@@ -22,11 +36,23 @@ def register():
     if not sport:
      return render_template("index.html",message="Not a valid ")
     else:
-     registrant_name[name]=sport
+        person=Sports(name=name,sport=sport)
+        db.session.add(person)
+        db.session.commit()
      
-     return render_template("success.html",context=context)
+    return render_template("success.html",context=context)
  
 
 @app.route('/registrants')
 def registrants():
-   return render_template("registrants.html",registrants=registrant_name)
+   persons = Sports.query.all()
+   return render_template("registrants.html",registrants=persons)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
